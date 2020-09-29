655adeb8a33d6317336025a0c0c5ced233520560
Tomasz Gądek <tomasz@eduj.pl>
2020-09-29 12:41:14 +0200

    poprawa url do zdjec

--- src/main/java/pl/ishop/portfolio/repository/PortfolioRepository.java
+++ src/main/java/pl/ishop/portfolio/repository/PortfolioRepository.java
@@ -15,15 +15,15 @@ public class PortfolioRepository {
     public PortfolioRepository() {
         products = new HashMap<>();
 
-        this.products.put(1, new Product(1, "Produkt 1", "Opis 1...", "http://dev-livecode.codenga.pl/portfolio-img/01.jpg", 1200));
-        this.products.put(2, new Product(2, "Produkt 2", "Opis 2...", "http://dev-livecode.codenga.pl/portfolio-img/02.jpg", 2000));
-        this.products.put(3, new Product(3, "Produkt 3", "Opis 3...", "http://dev-livecode.codenga.pl/portfolio-img/03.jpg", 1100));
-        this.products.put(4, new Product(4, "Produkt 4", "Opis 4...", "http://dev-livecode.codenga.pl/portfolio-img/04.jpg", 1300));
-        this.products.put(5, new Product(5, "Produkt 5", "Opis 5...", "http://dev-livecode.codenga.pl/portfolio-img/05.jpg", 1200));
-        this.products.put(6, new Product(6, "Produkt 6", "Opis 6...", "http://dev-livecode.codenga.pl/portfolio-img/05.jpg", 1100));
-        this.products.put(7, new Product(7, "Produkt 7", "Opis 7...", "http://dev-livecode.codenga.pl/portfolio-img/03.jpg", 2100));
-        this.products.put(8, new Product(8, "Produkt 8", "Opis 8...", "http://dev-livecode.codenga.pl/portfolio-img/02.jpg", 3000));
-        this.products.put(9, new Product(9, "Produkt 9", "Opis 9...", "http://dev-livecode.codenga.pl/portfolio-img/01.jpg", 1400));
+        this.products.put(1, new Product(1, "Produkt 1", "Opis 1...", "http://dev-livecode.codenga.pl/portfolio-img/product_1.png", 1200));
+        this.products.put(2, new Product(2, "Produkt 2", "Opis 2...", "http://dev-livecode.codenga.pl/portfolio-img/product_2.png", 2000));
+        this.products.put(3, new Product(3, "Produkt 3", "Opis 3...", "http://dev-livecode.codenga.pl/portfolio-img/product_3.png", 1100));
+        this.products.put(4, new Product(4, "Produkt 4", "Opis 4...", "http://dev-livecode.codenga.pl/portfolio-img/product_4.png", 1300));
+        this.products.put(5, new Product(5, "Produkt 5", "Opis 5...", "http://dev-livecode.codenga.pl/portfolio-img/product_5.png", 1200));
+        this.products.put(6, new Product(6, "Produkt 6", "Opis 6...", "http://dev-livecode.codenga.pl/portfolio-img/product_6.png", 1100));
+        this.products.put(7, new Product(7, "Produkt 7", "Opis 7...", "http://dev-livecode.codenga.pl/portfolio-img/product_7.png", 2100));
+        this.products.put(8, new Product(8, "Produkt 8", "Opis 8...", "http://dev-livecode.codenga.pl/portfolio-img/product_8.png", 3000));
+        this.products.put(9, new Product(9, "Produkt 9", "Opis 9...", "http://dev-livecode.codenga.pl/portfolio-img/product_9.png", 1400));
     }
 
     public List<Product> findAll() {
